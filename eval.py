70b4f79dce0da8b042adcc53ef4b61608aa2ca71
TongDuyDat <92315251+TongDuyDat@users.noreply.github.com>
2024-04-05 11:26:14 +0700

    update eval

--- eval.py
+++ eval.py
@@ -115,7 +115,7 @@ opt = parser.parse_args()
 
 # Load infos
 with open(opt.infos_path, 'rb') as f:
-    infos = cPickle.load(f)
+    infos = cPickle.load(f, cPickle.HIGHEST_PROTOCOL)
 
 # override and collect parameters
 if len(opt.input_fc_dir) == 0:
